bb20de4c5a00cd5be89fd8dc4716f8c9068c2a7f
Tajib Smajlović <34982684+TajibSmajlovic@users.noreply.github.com>
2018-04-01 16:12:43 +0200

    Add files via upload

---
+++ sql/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO TajibPC (department,manager) VALUES ('dep1','man1');
+INSERT INTO TajibPC (department,manager) VALUES ('dep2','man2');
+INSERT INTO TajibPC (department,manager) VALUES ('dep3','man3');
+INSERT INTO TajibPC (department,manager) VALUES ('dep4','man4');
+INSERT INTO TajibPC (department,manager) VALUES ('dep5','man5');
+INSERT INTO TajibPC (department,manager) VALUES ('dep6','man6');
+INSERT INTO TajibPC (department,manager) VALUES ('dep7','man7');
