069f25dcdfc1240221761a6290c00cd58604a26c
Amir Jamak <amirjamak@gmail.com>
2018-04-02 15:43:44 +0200

    Revert "Add files via upload"
    This reverts commit 72dd89a08928ccfac8b6024bc096a5a591c52b5f.

--- db.js
+++
@@ -1,7 +0,0 @@
-let connectionString = 'postgres://postgres:SteelSeries123@localhost:5432/postgres'
-let knex = require('knex')({
-  client: 'pg',
-  connection: connectionString,
-  acquireConnectionTimeout: 10000000
-})
-exports.model = require('bookshelf')(knex)
--- insert_user_table.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public."140302199" (id, department, manager) VALUES ('140302149','FENS/Electrial and Electronics Engineering','Emir Karamehmedovic');
-INSERT INTO public."140302199" (id, department, manager) VALUES ('140302049','FENS/Software Engineering','Enes Pivic');
-INSERT INTO public."140302199" (id, department, manager) VALUES ('140302065','FENS/Computer Science','Dzejla Medjedovic');
-INSERT INTO public."140302199" (id, department, manager) VALUES ('140302095','FENS/Mechanical Engineering','Muhamed Hadziabdic');
-INSERT INTO public."140302199" (id, department, manager) VALUES ('140302121','FENS/Genetics and Bioengineering','Ayla Arslan');
