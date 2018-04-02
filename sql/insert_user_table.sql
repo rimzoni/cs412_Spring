281de3b21d9ae7b33eef2af2813e4c66c0599a8e
Zehrudin <zude96@hotmail.com>
2018-04-02 12:24:23 +0200

    '150302075_table'

--- sql/insert_user_table.sql
+++ sql/insert_user_table.sql
@@ -1,1 +1,5 @@
-INSERT INTO public.User (name,email) VALUES ('john','john@gmail.com');
+INSERT INTO public."150302075" (id,department,manager) VALUES ('1','IT','Mirnes');
+INSERT INTO public."150302075" (id,department,manager) VALUES ('2','Director','Din');
+INSERT INTO public."150302075" (id,department,manager) VALUES ('3','Professor','Fadil');
+INSERT INTO public."150302075" (id,department,manager) VALUES ('4','Driver','Kenan');
+INSERT INTO public."150302075" (id,department,manager) VALUES ('5','Chef','Muamer');
