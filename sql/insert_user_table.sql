b2e66ec332134c20a05cb8378b6da6053f06978b
mirnes <mirnesmarshal@gmail.com>
2018-04-02 12:25:27 +0200

    '150302017_sql'

--- sql/insert_user_table.sql
+++ sql/insert_user_table.sql
@@ -1,1 +1,5 @@
-INSERT INTO public.User (name,email) VALUES ('john','john@gmail.com');
+INSERT INTO public.150302017 (id, department, manager) VALUES ('1','CS','mirnes');
+INSERT INTO public.150302017 (id, department, manager) VALUES ('2','CS','zehrudin');
+INSERT INTO public.150302017 (id, department, manager) VALUES ('3','SE','din');
+INSERT INTO public.150302017 (id, department, manager) VALUES ('4','SE','enes');
+INSERT INTO public.150302017 (id, department, manager) VALUES ('5','EE','almir');
