86c7f01b008cb600b0932a47640bafdf4c1fea4d
AljovicKenan <37076435+AljovicKenan@users.noreply.github.com>
2018-04-02 15:33:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,65 @@
-import UserDetail from './components/UserDetail'
-import Dashboard from './components/Dashboard'
-import Custom from './components/Custom'
+const express = require('express')
+const app = express()
+let bodyParser = require('body-parser')
+let cors = require('cors')
 
-import MyGrid from './components/MyGrid'
-import { BrowserRouter, Route } from 'react-router-dom'
+var Department = require('./department')
 
-const router = (
-          <Route location={location} path='/' exact component={Dashboard} />
-          <Route location={location} path='/users' exact component={UserGrid} />
-          <Route location={location} path='/user/:userId' exact component={UserDetail} />
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.text())
+app.use(bodyParser.json({type: 'application/json'}))
+app.use(cors())
 
-          <Route location = {location} path = '/MyGrid' exact component = {MyGrid}/>
-          <Route location={location} path='/custom' exact component={Custom} />
-          </div>
-        )}
+// in NodeJS/Express (server)
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Access-Control-Allow-Headers')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT')
+  next()
+})
+//to get all departments
+app.get('/departments', (req, res) => {
+	Department.all()
+		.then(function (departments) {
+		  res.status(200)
+			.json(departments)
+		})
+		.catch(function (err) {
+			console.log(err)
+			res.status(500).json({error: true, data: {error: err,
+        message: err.message}});
+		  })
+})
+//by name
+app.get('/ByDepartment', (req, res) => {
+  //console.log(req.query.department)
+  Department.byDepartment(req.query.department)
+		.then(function (departments) {
+		  res.status(200)
+			.json(departments)
+		})
+		.catch(function (err) {
+			console.log(err)
+			res.status(500).json({error: true, data: {error: err,
+        message: err.message}});
+		  })
+})
+//by id
+app.get('/ById', (req, res) => {
+	console.log(req.query.id);
+  let departmentId = req.query.id
+  Department.forge({id: departmentId}).fetch().then(function (departments) {
+    if (!departments) {
+      return res.status(404).json({ error: true, message: 'department not found' })
+    } else {
+      res.status(200).json(departments)
+    }
+  }).catch((err) => {
+    console.log(err)
+    res.status(500).json({error: true, data: {error: err, message: err.message}})
+  })
+})
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
