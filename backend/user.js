63f8c11c283f738feb1d9bb2d570ed7a98ed06ea
mirnes <mirnesmarshal@gmail.com>
2018-04-04 16:42:30 +0200

    20180404_150302017_midterm1

--- backend/user.js
+++ backend/user.js
@@ -1,13 +1,13 @@
 let bookshelf = require('./db')
 class User extends bookshelf.model.Model {
   get tableName () {
-    return 'User'
+    return 'UsersMirnes'
   }
   static all () {
     return this.forge().fetchAll()
   }
   static byName(name) {
-    return this.forge().query({where:{ 'User.name': name }}).fetchAll()
+    return this.forge().query({where:{ 'UsersMirnes.name': name }}).fetchAll()
   }
 }
 module.exports = User
