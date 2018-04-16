4a08902bcb3f365be6a59c953a65b5400646f76d
Amir Jamak <amirjamak@gmail.com>
2018-04-16 16:29:36 +0200

    week #9 monday, validation concepts

--- frontend/components/UserDetail.js
+++ frontend/components/UserDetail.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import axios from 'axios'
 import AppHeader from './Header'
-import { Button, Form, Container, Header } from 'semantic-ui-react'
+import { Button, Form, Container, Header, Message } from 'semantic-ui-react'
 
 
 class UserDetail extends Component {
@@ -12,7 +12,9 @@ class UserDetail extends Component {
       user: {
         name: '',
         email: ''
-      }
+      },
+      errorName: '',
+      errorEmail: ''
     }
     this.getUserById = this.getUserById.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -35,23 +37,44 @@ class UserDetail extends Component {
     axios.get('http://localhost:3000/user/delete?id='+userId)
         .then(this.props.history.push('/users'))
   }
+  validateFields(name, email){
+    let hasErrors = false
+    if(name.length <= 4) {
+      this.setState({errorName:'Name must be larger then 4 chars'})
+      hasErrors = true
+    } else {
+      this.setState({errorName:''})
+    }
+    let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let validateEmail= regex.test(email)
+    if(!validateEmail){
+      this.setState({errorEmail:'Email is not in valid format example@ius.ba'})
+      hasErrors = true
+    } else {
+      this.setState({errorEmail:''})
+    }
+    return hasErrors
+  }
   handleSubmit(e) {
     e.preventDefault()
   	e.stopPropagation()
     let name = e.target.name.value
     let email = e.target.email.value
+    let hasErrors = this.validateFields(name,email)
 
-    if(this.props.match.params.type == 'edit') {
-      axios.post('http://localhost:3000/user/update', {user: {
-        'id': this.props.match.params.userId,
-        'name': name,
-        'email': email
-      }}).then(response => this.setState({user: response.data}))
-    } else {
-      axios.post('http://localhost:3000/user/create', {user: {
-        'name': name,
-        'email': email
-      }}).then(response => this.setState({user: response.data}))
+    if(!hasErrors){
+      if(this.props.match.params.type == 'edit') {
+        axios.post('http://localhost:3000/user/update', {user: {
+          'id': this.props.match.params.userId,
+          'name': name,
+          'email': email
+        }}).then(response => this.setState({user: response.data}))
+      } else {
+        axios.post('http://localhost:3000/user/create', {user: {
+          'name': name,
+          'email': email
+        }}).then(response => this.setState({user: response.data}))
+      }
     }
   }
   handleChange(event,field) {
@@ -88,15 +111,29 @@ class UserDetail extends Component {
          <Form onSubmit={this.handleSubmit}>
           <Form.Field>
            <label>
-            Name:
-            <input type="text" name="name" value={this.state.user.name} onChange={(e) => this.handleChange(e, 'name')} />
+            Full Name:
+            <input type="text" name="name" required={true} value={this.state.user.name} onChange={(e) => this.handleChange(e, 'name')} />
             </label>
+           { this.state.errorName &&
+              <Message
+                color='red'
+                header="Full Name field error"
+                content={this.state.errorName}
+              />
+            }
           </Form.Field>
           <Form.Field>
            <label>
             Email:
-            <input type="text" name="email" value={this.state.user.email} onChange={(e) => this.handleChange(e, 'email')} />
+            <input type="text" name="email" required={true} value={this.state.user.email} onChange={(e) => this.handleChange(e, 'email')} />
            </label>
+            { this.state.errorEmail &&
+              <Message
+              color='red'
+                header="Email field error"
+                content={this.state.errorEmail}
+              />
+            }
           </Form.Field>
           <Button primary type='submit'>Submit</Button>
           <Button secondary className='button' onClick={(e) => this.removeUserById(e, this.state.user.id)}>Remove user</Button>
