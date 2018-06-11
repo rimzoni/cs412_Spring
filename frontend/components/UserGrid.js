cb040f3d26c8d65ba07ea9c38ef285fa0e081abd
Sara Spahic <sara_spahic@yahoo.com>
2018-06-11 17:18:03 +0200

    Final commit

--- frontend/components/UserGrid.js
+++ frontend/components/UserGrid.js
@@ -3,11 +3,27 @@ import { Link } from 'react-router-dom'
 //import './App.css'
 import Header from './Header'
 import axios from 'axios'
-import { Button, Form, Table, Container } from 'semantic-ui-react'
+import { Button, Form, Table, Container, Grid, Image } from 'semantic-ui-react'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { userActions } from '../actions'
+
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'olive',
+  'green',
+  'teal',
+  'blue',
+  'violet',
+  'purple',
+  'pink',
+  'brown',
+  'grey',
+  'black',
+]
 
 class UserGrid extends Component {
   constructor (props) {
@@ -57,31 +73,26 @@ class UserGrid extends Component {
         <Button type='submit'>Filter by name</Button>
        </Form>
        <br/>
-       <Table celled padded>
-        <Table.Header>
-         <Table.Row>
-          <Table.HeaderCell>Id</Table.HeaderCell>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Email</Table.HeaderCell>
-          <Table.HeaderCell>Token</Table.HeaderCell>
-          <Table.HeaderCell>Action</Table.HeaderCell>
-         </Table.Row>
-        </Table.Header>
-        <Table.Body>
+
+       <Grid celled padded columns={5} divided>
+        // <Table.Body>
          { this.props.users.map((user, key) => {
                    return (
-                      <Table.Row key={key}>
-                        <Table.Cell>{user.id}</Table.Cell>
-                        <Table.Cell>{user.name}</Table.Cell>
-                        <Table.Cell>{user.email}</Table.Cell>
-                        <Table.Cell>{user.token}</Table.Cell>
-                        <Table.Cell><Link to={'/user/edit/'+user.id}>edit</Link></Table.Cell>
-                      </Table.Row>
+                      <Grid.Row key={key}>
+                        <Grid.Column>{user.id}</Grid.Column>
+                        <Grid.Column>{user.name}</Grid.Column>
+                        <Grid.Column>{user.email}</Grid.Column>
+                        <Grid.Column>{user.token}</Grid.Column>
+                        <Grid.Column color='yellow'>
+                          <Link to={'/user/edit/'+user.id}>edit</Link>
+                        </Grid.Column>
+                      </Grid.Row>
                     )
                    })
          }
-         </Table.Body>
-        </Table>
+         // </Table.Body>
+        </Grid>
+
        </Container>
     )
   }
--- sql/insert_user_table.sql
+++ sql/insert_user_table.sql
@@ -1,1 +1,5 @@
-INSERT INTO public.User (name,email) VALUES ('john','john@gmail.com');
+INSERT INTO "public"."User" (name,email) VALUES ('aaaaa','aaaaa@gmail.com');
+INSERT INTO "public"."User" (name,email) VALUES ('bbbbb','bbbbb@gmail.com');
+INSERT INTO "public"."User" (name,email) VALUES ('ccccc','ccccc@gmail.com');
+INSERT INTO "public"."User" (name,email) VALUES ('ddddd','ddddd@gmail.com');
+INSERT INTO "public"."User" (name,email) VALUES ('aaaaa','aaaaa@gmail.com');
