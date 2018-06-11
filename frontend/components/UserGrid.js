6dc746e7223e660c107845027b24fea06ccacbd1
Lejla Hodzic <lejlasalic96@gmail.com>
2018-06-11 16:58:24 +0200

    Final exam

--- frontend/components/UserGrid.js
+++ frontend/components/UserGrid.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom'
 //import './App.css'
 import Header from './Header'
 import axios from 'axios'
-import { Button, Form, Table, Container } from 'semantic-ui-react'
+import { Button, Form, Table, Container, Grid, Image } from 'semantic-ui-react'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { userActions } from '../actions'
+import _ from 'lodash'
+
+const color = 'yellow';
 
 class UserGrid extends Component {
   constructor (props) {
@@ -57,31 +60,22 @@ class UserGrid extends Component {
         <Button type='submit'>Filter by name</Button>
        </Form>
        <br/>
-       <Table celled padded>
-        <Table.Header>
-         <Table.Row>
-          <Table.HeaderCell>Id</Table.HeaderCell>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Email</Table.HeaderCell>
-          <Table.HeaderCell>Token</Table.HeaderCell>
-          <Table.HeaderCell>Action</Table.HeaderCell>
-         </Table.Row>
-        </Table.Header>
-        <Table.Body>
+
+       <Grid columns={5} padded>
+ 
          { this.props.users.map((user, key) => {
                    return (
-                      <Table.Row key={key}>
-                        <Table.Cell>{user.id}</Table.Cell>
-                        <Table.Cell>{user.name}</Table.Cell>
-                        <Table.Cell>{user.email}</Table.Cell>
-                        <Table.Cell>{user.token}</Table.Cell>
-                        <Table.Cell><Link to={'/user/edit/'+user.id}>edit</Link></Table.Cell>
-                      </Table.Row>
+                      <Grid.Row key={key}>
+                        <Grid.Column>{user.id}</Grid.Column>
+                        <Grid.Column>{user.name}</Grid.Column>
+                        <Grid.Column>{user.email}</Grid.Column>
+                        <Grid.Column>{user.token}</Grid.Column>
+                        {(<Grid.Column color={color} key={key}><Link to={'/user/edit/'+user.id}>edit</Link></Grid.Column>)}
+                      </Grid.Row>
                     )
                    })
          }
-         </Table.Body>
-        </Table>
+        </Grid>
        </Container>
     )
   }
