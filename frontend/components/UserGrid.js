bdaa8483684b76e93347dabe97a6f672a590dcc4
ahmedtali <you@example.com>
2018-06-11 17:05:37 +0200

    Final

--- frontend/components/UserGrid.js
+++ frontend/components/UserGrid.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import Header from './Header'
 import axios from 'axios'
 import { Button, Form, Table, Container } from 'semantic-ui-react'
+import { Grid, Image } from 'semantic-ui-react'
+
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -57,34 +59,27 @@ class UserGrid extends Component {
         <Button type='submit'>Filter by name</Button>
        </Form>
        <br/>
-       <Table celled padded>
-        <Table.Header>
-         <Table.Row>
-          <Table.HeaderCell>Id</Table.HeaderCell>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Email</Table.HeaderCell>
-          <Table.HeaderCell>Token</Table.HeaderCell>
-          <Table.HeaderCell>Action</Table.HeaderCell>
-         </Table.Row>
-        </Table.Header>
-        <Table.Body>
-         { this.props.users.map((user, key) => {
-                   return (
-                      <Table.Row key={key}>
-                        <Table.Cell>{user.id}</Table.Cell>
-                        <Table.Cell>{user.name}</Table.Cell>
-                        <Table.Cell>{user.email}</Table.Cell>
-                        <Table.Cell>{user.token}</Table.Cell>
-                        <Table.Cell><Link to={'/user/edit/'+user.id}>edit</Link></Table.Cell>
-                      </Table.Row>
-                    )
-                   })
-         }
-         </Table.Body>
-        </Table>
-       </Container>
-    )
-  }
+       <Grid columns={5} divided>
+       { this.props.users.map((user, key) => {
+                return (
+                    <Grid.Row>
+                     <Grid.Column>{user.id}</Grid.Column>
+                     <Grid.Column>{user.name}</Grid.Column>
+                     <Grid.Column>{user.email}</Grid.Column>
+                     <Grid.Column>{user.token}</Grid.Column>
+                     <Grid.Column color='purple'><Link to={'/user/edit/'+user.id}>edit</Link></Grid.Column>
+                   </Grid.Row>
+                 )
+        })
+       }
+       </Grid>
+</Container>
+
+       )
+       }
+
+
+
 }
 const mapStateToProps = ({user}) => user
 const mapDispatchToProps = dispatch => {
